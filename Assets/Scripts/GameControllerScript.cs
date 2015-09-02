737bc53be4514b58338f91c59056acf1228e3a21
unknown <igcc@K003-5009.etudiant.rubika-edu.com.local>
2015-09-02 12:38:15 +0200

    New Framework

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+
+	public enum GOBBALL_TYPE {
+		GOBBALL_PINK,
+		GOBBALL_CYAN,
+		GOBBALL_ORANGE,
+		GOBBALL_RAINBOW
+	}
+
+	//public Text countdownTimerText;
+	//public Text numOfGobballLeftText;
+	public GameObject gobball;
+	public Sprite gobball_Pink;
+	public Sprite gobball_Cyan;
+	public Sprite gobball_Orange;
+	public Sprite gobball_Rainbow;
+	int numOfGobball = 10;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < numOfGobball; ++i) {
+			Vector3 gobballPosition = new Vector3 (Random.Range(-8.0f, 8.0f), Random.Range(-4.0f, 4.0f), 0);
+			GameObject newGobball = Instantiate (gobball, gobballPosition, Quaternion.identity) as GameObject;
+			newGobball.GetComponent<GobballSpawningScript>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GobballSpawningScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GobballSpawningScript : MonoBehaviour {
+
+	public enum GOBBALL_DIRECTION {
+		DIR_UP,
+		DIR_DOWN,
+		DIR_LEFT,
+		DIR_RIGHT
+	}
+
+	int type;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetGobballDirection(int dir) {
+
+	}
+
+	public void SetGobballType(int type, Sprite sprite) {
+	
+	}
+}
