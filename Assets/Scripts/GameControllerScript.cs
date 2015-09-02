e14f3f4dd56b157f0801a98ebcc7c0111fcb5c18
unknown <igcc@K003-5009.etudiant.rubika-edu.com.local>
2015-09-02 16:11:40 +0200

    (New) Random generated gobball (New) 3 huts

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -14,23 +14,24 @@ public class GameControllerScript : MonoBehaviour {
 	//public Text countdownTimerText;
 	//public Text numOfGobballLeftText;
 	public GameObject gobball;
-	public Sprite gobball_Pink;
-	public Sprite gobball_Cyan;
-	public Sprite gobball_Orange;
-	public Sprite gobball_Rainbow;
+	public Sprite[] gobballSprite;
 	int numOfGobball = 10;
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < numOfGobball; ++i) {
-			Vector3 gobballPosition = new Vector3 (Random.Range(-8.0f, 8.0f), Random.Range(-4.0f, 4.0f), 0);
-			GameObject newGobball = Instantiate (gobball, gobballPosition, Quaternion.identity) as GameObject;
-			newGobball.GetComponent<GobballSpawningScript>();
-		}
+		SpawningGobball ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void SpawningGobball() {
+		for (int i = 0; i < numOfGobball; ++i) {
+			Vector3 gobballPosition = new Vector3 (Random.Range(-8.0f, 8.0f), Random.Range(-4.0f, 4.0f), 0);
+			GameObject newGobball = Instantiate (gobball, gobballPosition, Quaternion.identity) as GameObject;
+			newGobball.GetComponent<SpriteRenderer>().sprite = gobballSprite[Random.Range (0, 3)];
+		}
 	}
 }
