1b3b2d8c5ced75ec6c24f91ba964a50d3df602f2
unknown <igcc@K003-5402.etudiant.rubika-edu.com.local>
2015-09-03 17:44:26 +0200

    score & clear time

--- Assets/Scripts/GameplayScript.cs
+++ Assets/Scripts/GameplayScript.cs
@@ -4,9 +4,10 @@ using System.Collections;
 
 public class GameplayScript : MonoBehaviour {
 
+	public int Count;
 	// Use this for initialization
 	void Start () {
-
+		Count = 0;
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/UI/CanvasUI.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class CanvasUI : MonoBehaviour {
+
+    //UI Objects  UIオブジェクト
+    private GameObject UIObject_CountDown;          //CountDown         カウントダウンタイマー
+    private GameObject UIObject_Panel;              //Panel             パネル
+    private GameObject UIObject_ClearTime;          //ClearTime         クリア時間
+    private GameObject UIObject_Score;              //Score             スコア
+    
+	public GameplayScript gamePlayScript;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        //Find Game Object
+        UIObject_CountDown      = GameObject.Find("CountDown");
+        UIObject_Panel          = GameObject.Find("Panel");
+        UIObject_ClearTime      = GameObject.Find("ClearTime");
+        UIObject_Score          = GameObject.Find("Score");
+
+
+ 
+
+        //Unnecessary Objects Disabling
+        UIObject_Panel.SetActive(false);
+        UIObject_ClearTime.SetActive(false);
+        UIObject_Score.SetActive(false);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        float CurrentTime = UIObject_CountDown.GetComponent<CountDownTimer>().GetCurrentTimeFloat();
+		int score = gamePlayScript.Count;
+        //if Once of the game has not been finished 
+        if (CurrentTime == 0.0f || score == 15)
+        {
+			//stop at cout down timer
+			UIObject_CountDown.GetComponent<CountDownTimer>().SetFlag(false);
+
+            UIObject_Panel.SetActive(true);
+            UIObject_ClearTime.SetActive(true);
+            UIObject_Score.SetActive(true);
+
+            //Calculates the time and score
+            float ClearTime = (UIObject_CountDown.GetComponent<CountDownTimer>().GetTimeLimit()-1) - UIObject_CountDown.GetComponent<CountDownTimer>().GetCurrentTimeFloat();
+
+            UIObject_ClearTime.GetComponent<Text>().text = "ClearTime  : " + ClearTime.ToString("N2") + "sec";
+			UIObject_Score.GetComponent<Text>().text = "Score  :"+"  "+ score.ToString();
+
+        }
+    
+    }
+}
